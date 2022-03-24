2fc15ef78c9b35ebf4177d5996c6194b4f7436e2
henriqueccmoraes <henriqueccmoraes@fac.pe.senac.br>
2022-03-24 20:25:03 -0300

    iniciando os exercicios

---
+++ exercico.ts
@@ -0,0 +1,22 @@
+// number
+
+let x: number = 10;
+let y: number = 20;
+console.log("Primeiro número: " + x);
+console.log("Segundo número: " + y);
+
+// 1 - Somar dois números informados como parâmetro;
+console.log("Soma igual: " + (x + y));
+//2 - Dividir dois números informados como parâmetro;
+console.log("Divisão igual: " + (y / x));
+//3 - Subtrair um número informado como parâmetro do outro;
+console.log("Subtração igual: " + (y - x));
+//4 - Multiplicar dois números informados como parâmetro;
+console.log("Multiplicação igual: " + (x * y));
+/*5 - Dada a quilometragem e a quantidade de litros, retornar o valor em Km/l de um veículo, 
+com a concatenação do "km/l" ao final;*/
+let km: number = 100;
+let litros: number = 10;
+console.log("Quantidade km: " + km);
+console.log("Quantidade litros: " + litros);
+console.log("O carro fez " + km / litros + " quilometros por litro");
---
+++ imc.html
@@ -0,0 +1,30 @@
+<html>
+<meta charset="UTF-8">
+
+<head> CÁLCULO DO IMC 
+<script type="text/javascript">
+function CalculoImc(){
+var peso = parseFloat(document.formImc.peso.value);
+var altura = parseFloat(document.formImc.altura.value);
+var imc = peso/(altura*altura);
+document.formImc.resultado.value = imc.toFixed(2);
+	
+}
+  
+</script>
+</head>
+<body>
+
+<form action="imc.html" name="formImc" method="post" onsubmit="return CalculoImc();">
+
+<p>Peso: <input type="text" name="peso"></p>
+<p>Altura: <input type="text" name="altura"></p>
+<p><input type="submit" value="Calcular">
+<input type="reset" value="Limpar"></p>
+<p>Resultado:<input type="text" name="resultado"></p>
+
+</form>
+
+	
+</body>
+</html>
