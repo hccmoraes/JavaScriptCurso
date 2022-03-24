4026723e65a7cd2f43bb4f4272fa1c6d25867c5e
henriqueccmoraes <henriqueccmoraes@fac.pe.senac.br>
2022-03-24 20:39:20 -0300

    exercicios realizados

---
+++ ex008.ts
@@ -0,0 +1,7 @@
+//console.log('O console fucionou corretamente!');
+var vel = 100.5;
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if(vel > 100){
+    console.log(`Você foi multado`);
+}
+console.log('Dirija sempre usando o cinto de segurança');
